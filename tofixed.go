19c04f9d758c83773469af6f38cca25832980098
qrawl <pierre@prinetti.it>
2015-01-14 00:24:58 +0100

    document format tag

--- tofixed.go
+++ tofixed.go
@@ -27,7 +27,7 @@ type Line []rune
 //		ValB int           `fixed:"10-20"`
 //		ValC *EmbeddedType `fixed:"20-50"`
 //		ValD time.Time     `fixed:"50-60,2006-01-02"`
-//
+//		ValE float         `fixed:"60-70,2"`
 // 	}
 //	type EmbeddedType struct {
 //		ValX string `fixed:"0-7"`
@@ -42,9 +42,8 @@ type Line []rune
 // String offsets are zero based.
 // Field filling is based on data type: for text types it will be spaces,
 // while numbers will be right-aligned and filled with zeroes.
-// Floating point-values are printed with two decimal positions by default.
-// TODO: Handle overlap
-// TODO: Handle pointers
+// Floating point-values are printed with the specified number of decimals (two by default).
+// time.Time fields are printed in the specified format.
 func Marshal(v interface{}) (string, error) {
 	var line Line // Build a rune array the length the output line is supposed to be
 	line = make([]rune, LineLength(v))
